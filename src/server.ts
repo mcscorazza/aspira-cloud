557c45421495b3c58cae896b2a0febcc00ed49da
Marcos Corazza <mcs.corazza@gmail.com>
2026-04-01 17:31:49 -0300

    fix: json bug

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,8 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
 
 const storage = multer.memoryStorage();
