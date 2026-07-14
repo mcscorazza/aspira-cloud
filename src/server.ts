30919adf11537d0858f8fbfd2f2702ed1960525a
Marcos Corazza <mcs.corazza@gmail.com>
2026-07-14 08:56:37 -0300

    bug fix

--- src/server.ts
+++ src/server.ts
@@ -41,7 +41,7 @@ const verificarTokenESP = (req: Request, res: Response, next: NextFunction) => {
 const CRC_TABLE = (() => {
   const t = new Uint32Array(256);
   for (let n = 0; n < 256; n++) {
-    let c = n + 55;
+    let c = n;
     for (let k = 0; k < 8; k++) c = c & 1 ? 0xedb88320 ^ (c >>> 1) : c >>> 1;
     t[n] = c >>> 0;
   }
